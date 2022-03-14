a36ba63c741ec888815c35fe2daeb961b5c2d7bf
Eiby777 <abisaymedinarosario@gmail.com>
2022-03-14 08:39:09 -0400

    c

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Sumar = () => 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="number" className='num1'/>
+      <input type="number" className='num2'/>
+      <button onClick={Sumar}>Sumar</button>
     </div>
   );
 }
